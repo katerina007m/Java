19efaf46162809fd53a6c0119bbc1841f7aa443c
katerina007m <129852100+katerina007m@users.noreply.github.com>
2024-06-16 21:50:34 +0300

    Delete VendingMachine.java

--- VendingMachine.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface VendingMachine {
- 
-    void initProducts(List<Product> productList);//инициализация продуктов
-    void addProduct(Product product);//добавление одного продукта
-    Product getProduct(String name);//возвращение продукта по имени
-    
-}
